ce1002ce621c629cd3298d6d08610a18afe1ab94
Jane Zhang <jane.zhang@hujiang.com>
2016-05-23 18:58:15 +0800

    config

--- src/config.js
+++ src/config.js
@@ -6,7 +6,8 @@
 
 //http://dev.mci.hujiang.com/teacher/v1/organs/{schoolId:int}
 
-export const API_ROOT = 'http://local.mci.hujiang.com/teacher/v1/';
+export const API_ROOT = 'dev.mci.hujiang.com/teacher/v1';
+//'http://local.mci.hujiang.com/teacher/v1/';
 //http://local.mci.hujiang.com/teacher/v1/
 export const HJPASSPORT_PATH = '//qapass.hujiang.com/m';
 
